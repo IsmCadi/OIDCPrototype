c71041c20d4d16b88d3db7adca5dbee90359e487
IsmCadi <ism_cadaroski@hotmail.com>
2023-04-20 15:21:00 +0200

    - added variables for auth

---
+++ prototype/src/main/java/OIDCAuthService.java
@@ -0,0 +1,25 @@
+package main.java;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+public class OIDCAuthService {
+    private final Logger log = Logger.getLogger(String.valueOf(OIDCAuthService.class));
+
+    // Client-info that has to be inserted manually atm
+    private final String clientID = "";
+    private final String clientSecret = "";
+    private final String redirectURI = "";
+
+
+    // KeycloakServer-info that has to be inserted manually atm
+    private final String authorizationEndpoint = "";
+    private final String tokenEndpoint = "";
+    private final String userInfoEndpoint = "";
+
+
+
+
+
+
+}
