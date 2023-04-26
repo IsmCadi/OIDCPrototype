f3b7b8aeb48d3b7401258ccd7addd61bad5f9b4f
IsmCadi <ism_cadaroski@hotmail.com>
2023-04-26 17:08:57 +0200

    - extended the exchangeAuthorizationCodeForAccessToken method to return the access aswell the id token and print them out as proof

--- prototype/src/main/java/ch/oidc/OIDCAuthorizationService.java
+++ prototype/src/main/java/ch/oidc/OIDCAuthorizationService.java
@@ -8,11 +8,14 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.http.*;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.util.Collections;
 
 public class OIDCAuthorizationService {
+
     private static final String clientID = "minio";
     private static final String clientSecret = "password";
     private static final String redirectURI = "http://192.168.1.65:9000/oidc/callback";
@@ -20,9 +23,14 @@ public class OIDCAuthorizationService {
     private static final String tokenEndpoint = "http://192.168.1.65:8080/auth/realms/cyberduckrealm/protocol/openid-connect/token";
     private static final String authorizationEndpoint = "http://192.168.1.65:8080/auth/realms/cyberduckrealm/protocol/openid-connect/auth";
 
+    // Sets up the HTTP transport and JSON factory
+    private final HttpTransport httpTransport = new NetHttpTransport();
+    private static final GsonFactory jsonFactory = new GsonFactory();
+
+    // Builds flow and trigger user authorization request
     public String getAuthorizationUrl() {
-        HttpTransport httpTransport = new NetHttpTransport();
-        GsonFactory jsonFactory = new GsonFactory();
+
+        // Sets up the authorization code flow
         AuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 httpTransport,
                 jsonFactory,
@@ -33,18 +41,35 @@ public class OIDCAuthorizationService {
                 .setApprovalPrompt("force")
                 .setAuthorizationServerEncodedUrl(authorizationEndpoint)
                 .build();
+        // Generates the authorization URL
         AuthorizationCodeRequestUrl authorizationUrl = flow.newAuthorizationUrl()
                 .setRedirectUri(redirectURI);
         return authorizationUrl.build();
     }
 
-    public String exchangeAuthorizationCodeForAccessToken(String authorizationCode) throws IOException {
+    // Exchanges the authorization code for an access token
+/*    public String exchangeAuthorizationCodeForAccessToken(String authorizationCode) throws IOException {
         TokenResponse response = new AuthorizationCodeTokenRequest(new NetHttpTransport(), new GsonFactory(),
                 new GenericUrl(tokenEndpoint), authorizationCode)
                 .setRedirectUri(redirectURI)
                 .setClientAuthentication(new BasicAuthentication(clientID, clientSecret))
                 .execute();
         return response.getAccessToken();
+    }*/
+
+    // Exchanges the authorization code for the access and id token
+    public static String exchangeAuthorizationCodeForAccessToken(String authorizationCode) throws IOException {
+        TokenResponse response = new AuthorizationCodeTokenRequest(new NetHttpTransport(), jsonFactory,
+                new GenericUrl(tokenEndpoint), authorizationCode)
+                .setRedirectUri(redirectURI)
+                .setClientAuthentication(new BasicAuthentication(clientID, clientSecret))
+                .execute();
+        // Gets the access and id token from the response
+        String accessToken = response.getAccessToken();
+        String idToken = response.get("id_token").toString();
+        System.out.println("Access token: " + accessToken);
+        System.out.println("ID token: " + idToken);
+        return accessToken;
     }
 
     public static void validateAccessToken(String accessToken) throws IOException {
