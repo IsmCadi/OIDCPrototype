b21e5ce6f25c172b42e47a663c16ba868d4f0410
Ismail_Cadaroski <ismail.cadaroski@students.fhnw.ch>
2023-05-22 14:03:14 +0200

    added the latest test with the additional bucket for the keycloak healthcheck and the required sleep duration

--- prototype/src/test/resources/healthcheck/healthcheck.sh
+++ prototype/src/test/resources/healthcheck/healthcheck.sh
@@ -1,28 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-KEYCLOAK_URL="http://localhost:8080/health/ready"
+set -eo pipefail
 
-# Delay before starting the healthcheck
-DELAY=5
-echo "Delaying healthcheck for $DELAY seconds..."
-sleep $DELAY
+# Replace `http://localhost:8080` with the appropriate Keycloak URL
+KEYCLOAK_URL="http://localhost:8080"
 
-# Retry logic for checking Keycloak's health
-MAX_RETRIES=30
-RETRY_DELAY=5
+# Make a GET request to Keycloak's server info endpoint
+response=$(wget --timeout=5 --tries=1 -qO- $KEYCLOAK_URL/auth/realms/master)
 
-# Performs the healthcheck
-for i in $(seq 1 $MAX_RETRIES); do
-  response=$(curl -s -o /dev/null -w "%{http_code}" "$KEYCLOAK_URL")
-
-  if [ "$response" = "200" ]; then
-    echo "Keycloak is ready."
-    exit 0
-  fi
-
-  echo "Keycloak is not ready yet. Retrying in $RETRY_DELAY seconds..."
-  sleep $RETRY_DELAY
-done
-
-echo "Keycloak healthcheck failed after $MAX_RETRIES retries."
-exit 1
+# Check if the response contains a specific string that indicates Keycloak is healthy
+if [[ $response == *"version"* ]]; then
+    exit 0 # Keycloak is healthy
+else
+    exit 1 # Keycloak is not healthy
+fi
